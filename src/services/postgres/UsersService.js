fc3905b3b6a1648481282acc890425566cf66300
hilmiseptian <hilmiseptian110@gmail.com>
2023-10-14 14:26:53 +0700

    authorization implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
-const AuthenticationsError = require('../../exceptions/AuthenticationsError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
   constructor() {
@@ -63,13 +63,13 @@ class UsersService {
  
     const result = await this._pool.query(query);
     if (!result.rows.length) {
-      throw new AuthenticationsError('Kredensial yang Anda berikan salah');
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
     }
     const { id, password: hashedPassword } = result.rows[0];
 
     const match = await bcrypt.compare(password, hashedPassword);
     if (!match) {
-      throw new AuthenticationsError('Kredensial yang Anda berikan salah');
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
     }
     return id;
   }
